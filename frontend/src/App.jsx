daa9a95c58760eee72efa9ca779b6d605a123083
Sanjh Dey Sarker <sanjhdeysarker@gmail.com>
2025-10-26 03:14:57 +0530

    app-chnages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
 import Dashboard from "./pages/Dashboard";
+import Settings from "./pages/Settings";
 
-function App() {
-  return <Dashboard />;
+export default function App() {
+  return (
+    <Router>
+      <div className="flex h-screen">
+        <Sidebar />
+        <div className="flex flex-col flex-1">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/settings" element={<Settings />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
 }
-
-export default App;
